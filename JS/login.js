a14dea1c6618a1e29a8fc93a1b43a463551e2cb7
RaselDev28 <rasel.dev28@gmail.com>
2025-02-14 07:34:21 +0600

    Prevent

---
+++ JS/login.js
@@ -0,0 +1,11 @@
+document.getElementById('button-login').addEventListener('click',function(event){
+    event.preventDefault();
+    console.log("button-login")
+
+
+    // get Phone-number
+
+const PhoneNumber=document.getElementById('Phone-number').value;
+console.log(PhoneNumber);
+})
+
