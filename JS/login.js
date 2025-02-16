2f27584a2b6f6b6f9519fee22e9a1bc6372f461d
RaselDev28 <rasel.dev28@gmail.com>
2025-02-17 00:03:33 +0600

    home page

--- JS/login.js
+++ JS/login.js
@@ -1,11 +1,22 @@
 document.getElementById('button-login').addEventListener('click',function(event){
     event.preventDefault();
-    console.log("button-login")
+    
 
 
     // get Phone-number
 
 const PhoneNumber=document.getElementById('Phone-number').value;
-console.log(PhoneNumber);
+// Get Pin Number
+const pinNumber=document.getElementById('pin-number').value;
+
+// console.log(PhoneNumber, pinNumber);
+
+if(PhoneNumber=== '01789853943' && pinNumber==='0148'){
+    console.log("You are loged in");
+    window.location.href="home.html";
+}
+else{
+    alert('Invalid Credentials');
+}
 })
 
