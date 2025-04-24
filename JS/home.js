d07b4fc71ae18af10e13be7515d5790894725f23
RaselDev28 <rasel.dev28@gmail.com>
2025-04-25 00:05:39 +0600

    update balence

--- JS/home.js
+++ JS/home.js
@@ -5,4 +5,29 @@ document.getElementById("btn-add-money")
         const addMoneyInput=document.getElementById('input-add-money').value;
 
         const pinNumber=document.getElementById('pin-number').value;
+
+        // Check Pin Number....But this is not a valid way
+
+        if(pinNumber === "0148"){
+            // console.log("Add Your money");
+
+            // Get the current Balence
+
+            const balence=document.getElementById('account-balence').innerText;
+            // console.log(balence);
+
+            // Add total Balence
+            const accountBalence=parseFloat(balence);
+            const newAmount=parseFloat(addMoneyInput);
+
+            const updatedBalence=accountBalence+newAmount;
+            console.log(updatedBalence);
+
+            // Updated UI/DOM
+
+            document.getElementById('account-balence').innerText=updatedBalence;
+        }
+        else{
+            alert("Pin Number is invalid");
+        }
     })
