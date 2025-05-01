c6b281ffaca9754b227086230e130382bcef32fe
RaselDev28 <rasel.dev28@gmail.com>
2025-05-02 01:02:56 +0600

    Function

--- Payoo_MFS_Function/js/add_money.js
+++ Payoo_MFS_Function/js/add_money.js
@@ -1,3 +1,8 @@
 document.getElementById("btn_add_money").addEventListener("click", function (event) {
     event.preventDefault();
+
+    
+const addMoney=getInputFieldValueById('input-add-money');
+const pinNumber=getInputFieldValueById('input-pin-number');
+console.log("Add Money is :", addMoney, pinNumber);
 })
--- Payoo_MFS_Function/js/utilities.js
+++ Payoo_MFS_Function/js/utilities.js
@@ -3,6 +3,11 @@
 
 */
 
-function getInputFieldById(){
-    
+// function getInputFieldValueById(){
+// //  const addMoney=document.getElementById("input-add-money").value;
+// //  return addMoney;   
+// }
+function getInputFieldValueById(id){
+const inputValue=document.getElementById(id).value;
+return inputValue;
 }
